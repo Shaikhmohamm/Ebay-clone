8b020536343467121adaeeae43a5a6383924ba92
zoha shaikh <zoha@pleximus.net>
2024-08-12 08:25:59 +0530

    added product-list-api

--- backend/controller/category.controller.js
+++ backend/controller/category.controller.js
@@ -4,6 +4,11 @@ dotenv.config()
 import category from '../data/categoryList.js'
 
 
+const BASE_URL = process.env.BASE_URL
+const API_KEY = process.env.API_KEY
+const HOST = process.env.HOST 
+
+
 export const getAllCategory = async (req, res) => {
     try {
         res.status(200).send(category)
@@ -11,3 +16,28 @@ export const getAllCategory = async (req, res) => {
         console.log("error" , error)
     }
 }
+
+
+export const getListOfProducts = async (req, res) => {
+    const { name, page = 0 } = req.query;
+  
+    try {
+        const response = await axios.get(`${BASE_URL}/item_search_2`, {
+            params: {
+              q: name,
+              page: page,
+              sort: 'default'
+            },
+            headers: {
+              "x-rapidapi-key": API_KEY,
+              "x-rapidapi-host": HOST
+            }
+          });
+  
+      res.json(response.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ error: 'Failed to fetch products' });
+    }
+  }
+  
--- backend/routes/category.router.js
+++ backend/routes/category.router.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import { getAllCategory } from '../controller/category.controller.js'
+import { getAllCategory, getListOfProducts } from '../controller/category.controller.js'
 
 const categoryRouter = express.Router()
 
 categoryRouter.get(`/category`, getAllCategory)
 
-
-
+categoryRouter.get(`/products`, getListOfProducts)
 
 
 
